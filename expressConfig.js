098e808ae131facb72688b47adde58ad305af199
HarryCZ <jakub.voboril@gmail.com>
2019-05-08 23:25:36 +0200

    CORS

--- expressConfig.js
+++ expressConfig.js
@@ -7,6 +7,14 @@ var express = require('express'),
     path = require('path');
 
 var rootPath = path.normalize(__dirname + '/../../');
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'example.com');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
 
 module.exports = function(app) {
   // app.use(logger('tiny'));
@@ -14,13 +22,14 @@ module.exports = function(app) {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(session({
-    secret: 'multi vision unicorns', 
+    secret: 'multi vision unicorns',
     resave:false,
     saveUninitialized: true
   }));
+  app.use(allowCrossDomain);
   app.use(passport.initialize());
   app.use(passport.session());
-  
+
   app.use(express.static(rootPath));
   app.use(express.static(rootPath + '/dist'));
   app.use('/events', express.static(rootPath));
